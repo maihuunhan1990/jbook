f98be0ed40beeb2c5064a6c0b9ccff8c99188155
Nhan <maihuunhan1990@gmail.com>
2022-01-31 22:32:00 -0500

    add reducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
@@ -18,37 +19,73 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
+//produce is from immer
+const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
-      return {
-        //create new object that has all existing property of state
-        ...state,
-        //redefine the data property
-        data: {
-          //has all existing sate data property
-          ...state.data,
-          //overwrite whatever is at action.payload.id
-          [id]: {
-            ...state.data[id],
-            content,
-          },
-        },
-      };
-    case ActionType.DELETE_CELL:
+      //immer will return the state object for us
+      //so no need for us to return like the code snip below
+      //only returning because of typescript
+      state.data[id].content = content;
       return state;
+    //would need to do this if we didn't use immer
+    // return {
+    //   //create new object that has all existing property of state
+    //   ...state,
+    //   //redefine the data property
+    //   data: {
+    //     //has all existing sate data property
+    //     ...state.data,
+    //     //overwrite whatever is at action.payload.id
+    //     [id]: {
+    //       ...state.data[id],
+    //       content,
+    //     },
+    //   },
+    // };
+    case ActionType.DELETE_CELL:
+      delete state.data[action.payload];
+      state.order.filter((id) => id !== action.payload);
+      return;
 
     case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return state;
+      }
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
       return state;
     case ActionType.INSERT_CELL_BEFORE:
+      const cell: Cell = {
+        content: '',
+        type: action.payload.type,
+        id: randomId(),
+      };
+
+      state.data[cell.id] = cell;
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+      if (foundIndex < 0) {
+        //push the cell to the push because no id was specified
+        state.order.push(cell.id);
+      } else {
+        //doc on npmjs immer doc
+        state.order.splice(foundIndex, 0, cell.id);
+      }
+
       return state;
     default:
       return state;
   }
-};
+});
 
+const randomId = () => {
+  //using base 36, string will use strings and number
+  return Math.random().toString(36).substring(2, 5);
+};
 export default reducer;
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,25 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { ActionType } from './action-types';
 import reducers from './reducers';
 
 // {} empty object for intial state
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code',
+  },
+});
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code',
+  },
+});
+
+console.log(store.getState());
